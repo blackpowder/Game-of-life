3cb1f7ff75004cb13605380d3fb6be56f618d0e0
Joshua Vial <joshua@enspiral.com>
2013-09-13 12:57:02 +0700

    breaking into small steps

--- game_of_life.rb
+++ game_of_life.rb
@@ -11,77 +11,35 @@ class Game_of_life
     end
   end
 
+  #call next turn and save the outputs to a new file
   def start
-    #call next turn and save the outputs to a new file
     p @board
     new_board = next_turn(@board)
     p new_board
+    #todo save new board to a file called new_board.txt
   end
 
   #create a new board with the updated values from one turn
   def next_turn(board)
-    #create a new board with the same dimensions as the old board
-    #new_board = ...
+    return board
 
-    #iterate over each cell in the board and calculate it's value by calling next_cell_value
-    board.each_with_index do |cells, row|
-      cells.each_with_index do
-       |cell, column|
-
-       @cell = cell
-       @row = row
-       @column = column  
-     
-       next_cell_value(row,column)
-        #save the value in new_board variable at the same row, column
-
-       File.open("new_board.txt", "w+") do |f|    #to_do fix "w+"
-       f.write(@test)
-       end
-
-       @board.each do |new_cell|
-       @test = new_cell.map { |p| p }.join("")
-       end 
-      end
-    end
-    #return new_board
+    #  #todo create a new board with the same dimensions as board
+    #  #hint: google for dup method
+    #  #new_board = ...
+    #
+    #  #iterate over each cell in the board and calculate it's value by calling next_cell_value
+    #  board.each_with_index do |cells, row|
+    #    cells.each_with_index do |cell, column|
+    #      #todo save the next cell value in new_board variable at the same row, column
+    #      next_cell_value(board,row,column)
+    #     end
+    #  end
+    #
+    # return new_board
   end
 
-  #calculate the next value for a particular cell
-  def next_cell_value(x,y)
-    neighbours(@x,@y)  #get neighbours
-
-    #apply rules
-    #return cell value
-  end
-
-  #return an array with the values of the cells neighbouring to the position x,y
-  def neighbours(x,y)
-    x = @row
-    y = @column 
-   
-    puts "x = #{x} and y = #{y}"
-
-
-  
-
-
-
-  
-
-  
-
-
-    #if x,y is in the middle of the board this should return 8 cells
-    #if x,y is in a corner it should return 3
-    #if x,y is in the edge of a row it should 5
-
-    #test it works for all four corners of the board
-    #puts @board[3][4]  #wrote it, just to test, next time will work on this method
-
-    #return an array with the values of the neighbouring cells, don't worry about the positions
-    # e.g. [0,0,0] would be a corner cell with neighbours that are not alive
-    # [0,*,0] would have one neighbour alive etc. - it doesn't matter which order or position each cell is in
+  def next_cell_value(board, row, column)
+    '0'
   end
 end
 
@@ -89,5 +47,59 @@ mygame = Game_of_life.new
 mygame.start
 
 
-
-
+#old stuff
+#create a new board with the updated values from one turn
+#def next_turn(board)
+#  #todo create a new board with the same dimensions as the old board
+#  #new_board = ...
+#
+#  #iterate over each cell in the board and calculate it's value by calling next_cell_value
+#  board.each_with_index do |cells, row|
+#    cells.each_with_index do |cell, column|
+#
+#      @cell = cell
+#      @row = row
+#      @column = column
+#
+#      #todo save the next cell value in new_board variable at the same row, column
+#      next_cell_value(board,row,column)
+#
+#      File.open("new_board.txt", "w+") do |f|    #to_do fix "w+"
+#        f.write(@test)
+#      end
+#
+#      @board.each do |new_cell|
+#        @test = new_cell.map { |p| p }.join("")
+#      end
+#    end
+#  end
+#
+#  #todo return new_board
+#end
+#
+##calculate the next value for a particular cell
+#def next_cell_value(board, row, column)
+#  neighbours(board,row,column)  #get neighbours
+#
+#  #apply rules
+#  #return cell value
+#end
+#
+##return an array with the values of the cells neighbouring to the position x,y
+#def neighbours(board,row,column)
+#  x = @row
+#  y = @column
+#
+#  puts "row = #{row} and column = #{column}"
+#
+#  #if x,y is in the middle of the board this should return 8 cells
+#  #if x,y is in a corner it should return 3
+#  #if x,y is in the edge of a row it should 5
+#
+#  #test it works for all four corners of the board
+#  #puts @board[3][4]  #wrote it, just to test, next time will work on this method
+#
+#  #return an array with the values of the neighbouring cells, don't worry about the positions
+#  # e.g. [0,0,0] would be a corner cell with neighbours that are not alive
+#  # [0,*,0] would have one neighbour alive etc. - it doesn't matter which order or position each cell is in
+#end
