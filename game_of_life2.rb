207715e3561355ead81a11b31a8914ee97e16a2a
Stephanie Noirpoudre <stephanie.noirpoudre@gmail.com>
2013-09-19 07:02:11 +0200

    starting of new shot of game of life, just wrote methods and todo

---
+++ game_of_life2.rb
@@ -0,0 +1,29 @@
+class Game_of life
+
+	def initialize
+		#todo open file
+		#todo get board
+	end
+
+	def start
+		#todo call get_next_generation some numbers of time
+
+		#todo board to new text file
+		#todo close text file 
+	end
+
+	def get_next_generation
+		#todo loop of each cell
+		#todo for each cell : call get_neighbourhs
+		
+		#todo rules 
+		#print each new cells onto new board at location x,y
+		#return back to start
+	end
+
+	def get_neighbours
+		#todo get the location (x, y) of each neighbours
+		#todo add neighbours if they are not off the board 
+		#return neighbours back to GET_NEXT_GENERATION
+	end
+end
